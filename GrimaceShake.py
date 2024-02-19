60fcfc3dcba6ee9af3df2b09eeb83cf0841b52e5
Psimmons <pjs@cesimmons.com>
2024-02-19 14:25:56 -0500

    Tested Variables Random Uniforms

--- GrimaceShake.py
+++ GrimaceShake.py
@@ -16,6 +16,17 @@ def listOfGasStations():
     gasStationsNearby = random.choice(gasStations)
     return gasStationsNearby
 
+#Function will call the gasLevelGauge to determine our gas level and then findn a close gas station
+#by calling the listOfGasStations function if we are on Low or Quarter Tank
+def gasLevelAlert():
+    milesToGasStationsLow = random.uniform(1,25)
+    milesToGasStationsQuarterTank = random.uniform(25.1,50)
+    #gasLevelGauge =  gasLevelGauge()
+    print(milesToGasStationsLow)
+    print(milesToGasStationsQuarterTank)
 
-print(gasLevelGauge())
-print(listOfGasStations())
+gasLevelAlert()
+
+
+
+
